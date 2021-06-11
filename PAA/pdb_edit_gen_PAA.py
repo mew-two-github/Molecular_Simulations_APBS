d117073ea0a771061bf747d08c283e47286d5a47
vishal <subhavishal@yahoo.co.in>
2021-06-11 17:21:51 +0530

    Now it adds Na+ ions

--- PAA/pdb_edit_gen_PAA.py
+++ PAA/pdb_edit_gen_PAA.py
@@ -23,6 +23,7 @@ def main(nRU,file_init, file_new,ion):
     # print(end_lines)
     # print(lines[0])
 
+    # Obtaining locations of the atoms (as a string)
     C_matrix = []
     for line in lines:
         C_matrix.append(line[31:55])
@@ -30,6 +31,7 @@ def main(nRU,file_init, file_new,ion):
     #print((C_matrix[0].split()))
     a = np.empty(shape=(Tatms,3))
 
+# Converting the string to (x,y,z) indices
     for i in range(len(C_matrix)):
         [x,y,z] = C_matrix[i].split()
         a[i] = np.asarray([x,y,z],dtype='float64')
@@ -52,14 +54,13 @@ def main(nRU,file_init, file_new,ion):
                 fout.write('ATOM    %3d  %2s  PAAH  %3d    %8.3f%8.3f%8.3f  1.00  0.00           %s\n'%(i+1,AT[i%n_atoms],(i//n_atoms+1),a[i][0],a[i][1],a[i][2],ET[i%n_atoms]))        
     else:
         for i in range(Tatms):
-            AT = ['C1','C2','CA','O1','O2','HA']
-            ET=['C','C','C','O','O','H']
-            if i < n_atoms:
-                fout.write('ATOM    %3d  %2s  PAHb  %3d    %8.3f%8.3f%8.3f  1.00  0.00           %s\n'%(i+1,AT[i%n_atoms],(i//n_atoms+1),a[i][0],a[i][1],a[i][2],ET[i%n_atoms]))
-            elif i >= Tatms-n_atoms:
-                fout.write('ATOM    %3d  %2s  PAHe  %3d    %8.3f%8.3f%8.3f  1.00  0.00           %s\n'%(i+1,AT[i%n_atoms],(i//n_atoms+1),a[i][0],a[i][1],a[i][2],ET[i%n_atoms]))
-            else:
-                fout.write('ATOM    %3d  %2s  PAAH  %3d    %8.3f%8.3f%8.3f  1.00  0.00           %s\n'%(i+1,AT[i%n_atoms],(i//n_atoms+1),a[i][0],a[i][1],a[i][2],ET[i%n_atoms])) 
+            AT = ['CFF','CFG','CFM','OFS','OFS']
+            ET=['C','C','C','O1-','O1-']
+            fout.write('ATOM    %3d  %3s  PDB  %3d    %8.3f%8.3f%8.3f  1.00  0.00           %s\n'%(i+1,AT[i%n_atoms],(i//n_atoms+1),a[i][0],a[i][1],a[i][2],ET[i%n_atoms]))
+        fout.write('TER {}\n'.format(Tatms+1))
+        for i in range(nRU):
+            coords = a[(i+1)*5-1]*0.975
+            fout.write('HETATM {:3d} NA NA {:3d} {:8.3f} {:8.3f} {:8.3f} 1.00 0.00           NA+\n'.format(Tatms+i+2,(i//n_atoms+1),coords[0],coords[1],coords[2]))
     for i in range(len(end_lines)):
         fout.write(end_lines[i])
     fout.close()
