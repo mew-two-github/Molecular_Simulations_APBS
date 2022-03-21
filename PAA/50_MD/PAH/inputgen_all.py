c599cf3f3de17e8d5bc7e8de0d17500cf813282f
vishal <subhavishal@yahoo.co.in>
2022-03-21 17:06:59 +0530

    Ran PAH50 properly

--- PAA/50_MD/PAH/inputgen_all.py
+++ PAA/50_MD/PAH/inputgen_all.py
@@ -5,19 +5,17 @@ def generate():
         subprocess.run(["python","inputgen.py","conf"+str(i)+"_edited.pdb"])
 
 def run():
-    for i in range(1): 
+    for i in range(11): 
         lol = subprocess.run(["apbs","conf"+str(i)+"_edited.in"],capture_output=True)
-#        print(lol.stdout.decode("utf-8"))
-#        break
         with open('output.txt','a') as file:
             file.write(lol.stdout.decode("utf-8"))
-        with open('output.txt','r') as file:
-            lines = file.readlines()
-        with open('energies.txt','a') as file:
-            for line in lines:
-                key_string = "  Global net ELEC energy"
-                if line[0:len(key_string)] == key_string:
-                    file.write(line)
+with open('output.txt','r') as file:
+    lines = file.readlines()
+with open('energies.txt','a') as file:
+    for line in lines:
+        key_string = "  Global net ELEC energy"
+        if line[0:len(key_string)] == key_string:
+            file.write(line)
 generate()
 run()
 
