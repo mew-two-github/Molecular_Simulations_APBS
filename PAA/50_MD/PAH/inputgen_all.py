84184f52492652f38f87ba9b86f36b672eefa39c
vishal <subhavishal@yahoo.co.in>
2022-03-21 16:50:46 +0530

    now write global net elec energy in a separate txt file

--- PAA/50_MD/PAH/inputgen_all.py
+++ PAA/50_MD/PAH/inputgen_all.py
@@ -2,16 +2,22 @@ import subprocess
 
 def generate():
     for i in range(11):
-        subprocess.run(["python","inputgen.py","conf"+str(i)+".pdb"])
+        subprocess.run(["python","inputgen.py","conf"+str(i)+"_edited.pdb"])
 
 def run():
-    for i in range(11): 
-        lol = subprocess.run(["apbs","conf"+str(i)+".in"],capture_output=True)
+    for i in range(1): 
+        lol = subprocess.run(["apbs","conf"+str(i)+"_edited.in"],capture_output=True)
 #        print(lol.stdout.decode("utf-8"))
 #        break
         with open('output.txt','a') as file:
             file.write(lol.stdout.decode("utf-8"))
-
+        with open('output.txt','r') as file:
+            lines = file.readlines()
+        with open('energies.txt','a') as file:
+            for line in lines:
+                key_string = "  Global net ELEC energy"
+                if line[0:len(key_string)] == key_string:
+                    file.write(line)
 generate()
-# run()
+run()
 
